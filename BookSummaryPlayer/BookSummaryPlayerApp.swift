63d3aed86a7c20b217e69a8295948a42b6354bee
Serhii Bets <serjbets@gmail.com>
2025-04-26 21:28:34 +0200

    Initial Commit

---
+++ BookSummaryPlayer/BookSummaryPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummaryPlayerApp.swift
+//  BookSummaryPlayer
+//
+//  Created by Serhii Bets on 26/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BookSummaryPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookSummaryPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookSummaryPlayer
+//
+//  Created by Serhii Bets on 26/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
